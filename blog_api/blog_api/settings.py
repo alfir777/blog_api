9a319ac8e1efb6912025f4902af739b85cede6f9
Alfir Tyakin <alfir777@rambler.ru>
2022-04-17 21:58:37 +0500

    add add django-mptt

--- blog_api/blog_api/settings.py
+++ blog_api/blog_api/settings.py
@@ -49,6 +49,7 @@ INSTALLED_APPS = [
 
     'rest_framework',
     'drf_yasg',
+    'mptt',
 
     'core.apps.CoreConfig',
 ]
--- blog_api/core/models.py
+++ blog_api/core/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from mptt.fields import TreeForeignKey
+from mptt.models import MPTTModel
 
 
 class Article(models.Model):
@@ -11,15 +13,21 @@ class Article(models.Model):
     def __str__(self):
         return self.title
 
+    def get_count_comments(self):
+        return f"{self.comments.all().count()}"
 
-class Comment(models.Model):
+
+class Comment(MPTTModel):
     article = models.ForeignKey(Article, on_delete=models.CASCADE, related_name='comments')
     content = models.TextField(blank=False)
-    parent = models.ForeignKey('self', on_delete=models.SET_NULL, null=True, blank=True, related_name='replies')
+    parent = TreeForeignKey('self', on_delete=models.SET_NULL, null=True, blank=True, related_name='children')
 
     class Meta:
         verbose_name = 'Comment'
         verbose_name_plural = 'Comments'
+
+    class MPTTMeta:
+        order_insertion_by = ['id']
 
     def __str__(self):
         return f'Comment {self.pk} by "{self.article}'
